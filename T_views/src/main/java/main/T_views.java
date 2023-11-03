24d14045c8ea3eb799634747c501c3e972d317f7
LoanWeefos <luis.duran233267@potros.itson.edu.mx>
2023-11-02 23:05:51 -0700

    Views sin ventana del juego.
    ✌️

---
+++ T_views/src/main/java/main/T_views.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import org.itson.t_views.frmInicio;
+
+/**
+ *
+ * @author Equipo6
+ */
+public class T_views {
+
+    public static void main(String[] args) {
+        frmInicio frame = frmInicio.getInstance();
+        frame.setVisible(true);
+    }
+}
